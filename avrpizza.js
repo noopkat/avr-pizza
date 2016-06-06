dd1bd6dfc22e3b0ebd806de15c5a4ee5a16dca66
Suz Hinton <suz.hinton@gmail.com>
2016-06-05 22:31:56 -0500

    pack tarball in parallel

--- avrpizza.js
+++ avrpizza.js
@@ -3,6 +3,7 @@ var path = require('path');
 var glob = require('glob');
 var fs = require('fs');
 var tar = require('tar-stream');
+var async = require('async');
 
 module.exports = {};
 
@@ -12,52 +13,109 @@ module.exports.compile = function avrpizza(package, callback) {
   var board = package.board || 'uno';
   var version = package.version || '10609';
 
-  // perpare a new tarball packing
+  // if user supplies a string path, just convert it into an array without burdening them with an error
+  if (typeof libpaths === 'string') {
+    libpaths = [libpaths];
+  }
+
+  // create array of paths to iterate on
+  // end result looks something like:
+  // [
+  //   ['/path/to/sketch/dir', 'sketch'],
+  //   ['/path/to/lib1', 'libs'],
+  //   ['/path/to/lib2', 'libs']
+  // ]
+  // push sketch dir first
+  var globPaths = [[path.dirname(sketchfile), 'sketch']];
+  // if there are libraries included, push them to list
+  libpaths.forEach(function(lib) {
+    globPaths.push([lib, 'libs']);
+  });
+
+  // prepare a new tarball packing
   var pack = tar.pack();
 
   // we def needs these top level dirs, let's guarantee that happens
   pack.entry({name: 'libs', type: 'directory'});
   pack.entry({name: 'dist', type: 'directory'});
   pack.entry({name: 'sketch', type: 'directory'});
+  
+  async.each(globPaths, findAndPackDependencies, function(error) {
+    if (error) return callback(error);
 
-  if (!Array.isArray(libpaths)) return callback(new Error('`libraries` option should be an array'));
+    // finally, pack the sketch file itself
+    packSketch(sketchfile, function(error) {
+      if (error) return callback(error);
 
-  // glob library and sketch file dirs
-  libpaths.forEach(function(libpath) {
+      // we're done with the tarball
+      pack.finalize();
+
+      // now we're ready to request compilation by throwing over the tarball to the request side
+      requestComp({files: pack, board: board, version: version, service: package.service}, function(error, data) {
+        var result = error ? null : new Buffer(data.data.src);
+
+        return callback(error, result);
+      });
+    });
+  });
+
+  function findDependencies(location, callback) {
+    var searchPath = location[0];
+    var destPath = location[1];
+
     // look for needed files within library dir
-    var matches = glob.sync("**/{*.h,*.cpp,*.c}", {cwd: libpath, root: path.join(libpath, '/'), matchBase: true});
-    if (!matches.length) return callback(new Error('No library files found in supplied path: ' + libpath));
+    // TODO: look into changing root in options to full path
+    glob("**/{*.h,*.cpp,*.c}", {cwd: searchPath, root: path.join(searchPath, '/'), matchBase: true}, function(error, matches) {
+      var error = null;
+      if (!matches.length && destPath === 'libs') error = new Error('No library files found in supplied path: ' + searchPath);
+
+      return callback(error, matches);
+    });
+  }
+
+  function packDependencies(matches, location, callback) {
+    if (!matches.length) return callback(null);
+
+    var basepath = location[0];
+    var destpath = location[1];
 
     // save each file into the tarball pack
-    matches.forEach(function(filepath) {
-      var libName = path.basename(libpath);
-      var filecontent = fs.readFileSync(path.join(libpath, filepath), {encoding: 'utf8'});
-      pack.entry({name: path.join('libs', libName, filepath)}, filecontent);
+    matches.forEach(function(filepath, index, arr) {
+
+      var libName = path.basename(basepath);
+      fs.readFile(path.join(basepath, filepath), {encoding: 'utf8'}, function(error, filecontent) {
+        if (error) return callback(error);
+
+        // pack file into tarball
+        pack.entry({name: path.join(destpath, libName, filepath)}, filecontent);
+
+        // return async if we're done
+        if (arr.length - 1 === index) return callback(null);
+
+      });
     });
-  });
+  }
 
-  var sketchpath = path.dirname(sketchfile);
-  // glob through the sketch directory to find likely file deps
-  var matches = glob.sync("**/{*.h,*.cpp,*.c}", {cwd: sketchpath, root: path.join(sketchpath, '/'), matchBase: true});
+  function findAndPackDependencies(location, callback) {
+    // find files that we can pack into the tarball for compilation
+    findDependencies(location, function(error, matches) {
+      if (error) return callback(error);
 
-  // save each file into the tarball pack
-  matches.forEach(function(filepath) {
-    var filecontent = fs.readFileSync(path.join(sketchpath, filepath), {encoding: 'utf8'});
-    pack.entry({name: path.join('sketch', filepath)}, filecontent);
-  });
-
-  // save the main sketch file to a special filename that the compiler can look for
-  fs.readFile(sketchfile, {encoding: 'utf8'}, function(error, sketchfile) {
-    if (error) return callback(new Error(error.message));
-    pack.entry({name: path.join('sketch', 'sketch.ino')}, sketchfile);
-
-    // we're done with the tarball
-    pack.finalize();
-
-    // now we're ready to request compilation by throwing over the tarball to the request side
-    requestComp({files: pack, board: board, version: version, service: package.service}, function(error, data) {
-      var result = error ? null : new Buffer(data.data.src);
-      return callback(error, result);
+      // send matching files to be packed into tarball
+      packDependencies(matches, location, function(error) {
+        return callback(error);
+      });
     });
-  });
+  }
+
+  function packSketch(sketchfile, callback) {
+    // save the main sketch file to a special filename that the compiler can look for
+    fs.readFile(sketchfile, {encoding: 'utf8'}, function(error, sketchfile) {
+      if (error) return callback(new Error(error.message));
+
+      pack.entry({name: path.join('sketch', 'sketch.ino')}, sketchfile);
+
+      return callback(null);
+    });
+  }
 };
